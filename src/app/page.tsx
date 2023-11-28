193998860b205bf4c1350a4a1e9e835355ff276d
Cesar Pedetta <cesarpedetta@gmail.com>
2023-11-28 18:18:58 -0300

    adding styles to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import { RandomFox } from "./components/RandomFox"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold underline">My Beautiful Fox</h1>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <p className="text-md text-blue-700 py-2">CURSO REACT CON TYPESCRIPT</p>
+      <p className="text-3xl font-bold py-2">Componente Lazy Image</p>
+      <p className="text-center pt-2 pb-4">Un componente genérico de React para cargar imágenes con lazy loading.</p>
       <RandomFox />
     </main>
   )
