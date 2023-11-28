da7600cfcc1d15b18a9a65afd3fd68b6aa47a50a
Cesar Pedetta <cesarpedetta@gmail.com>
2023-11-28 19:20:53 -0300

    defined types to useState and refactored components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,17 @@ import { RandomFox } from "./components/RandomFox"
 
 const random = (): number => Math.floor(Math.random() * 123) + 1;
 
+const generateId = () => Math.random().toString(36).substring(2, 9);
+
+type ImageItem = { id: string, url: string};
+
 export default function Home() {
   
-  const [images, setImages] = useState<string[]>([
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
+  const [images, setImages] = useState<Array<ImageItem>>([
+    {id: generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
+    {id: generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
+    {id: generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
+    {id: generateId(), url:`https://randomfox.ca/images/${random()}.jpg`},
   ]);
 
   return (
@@ -18,9 +22,9 @@ export default function Home() {
       <p className="text-md text-blue-700 py-2">CURSO REACT CON TYPESCRIPT</p>
       <p className="text-3xl font-bold py-2">Componente Lazy Image</p>
       <p className="text-center pt-2 pb-4">Un componente genérico de React para cargar imágenes con lazy loading.</p>
-      {images.map((image, index) => (
-        <div key={index} className="p-4">
-          <RandomFox image={image} />
+      {images.map(({ id, url }) => (
+        <div key={id} className="p-4">
+          <RandomFox image={url} />
         </div>
       ))}
     </div>
