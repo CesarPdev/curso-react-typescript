3179c9c8943946f6f5d67e6230fc602de964b284
Cesar Pedetta <cesarpedetta@gmail.com>
2023-11-30 18:17:41 -0300

    deleted redundant comments on LazyImage, present on RandomFox

--- src/app/components/LazyImage.tsx
+++ src/app/components/LazyImage.tsx
@@ -1,34 +1,6 @@
 import { useRef, useEffect, useState } from "react"
 import type { ImgHTMLAttributes } from "react"
 
-/* La forma implícita de declarar una variable en TypeScript es la misma de JavaScript:
-
-    export const RandomFox = () => {
-        return <img />
-    }
-
-Esto no se recomienda por ser una forma IMPLÍCITA de declaración
-
-*/
-
-/* Una manera de hacer explícita la declaración de la función o variable sería con el uso de FunctionComponent o FC de React:
-
-    import { FunctionComponent, FC } from 'react'
-
-    export const RandomFox: FunctionComponent = () => {
-        return <img />
-    }
-Esto actualmente no se utiliza por utilizar "ayudantes" de React innecesarios
-*/
-
-/* La forma explícita de declarar una función sería:
-
-export const RandomFox = (): JSX.Element => {
-    return <img />
-}
-
-Esta es la forma que utilizaremos en el curso */
-
 type LazyImageProps = {
     src: string
     onLazyLoad?: (img: HTMLImageElement) => void
